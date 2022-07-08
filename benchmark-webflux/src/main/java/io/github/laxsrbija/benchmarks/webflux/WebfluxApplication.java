6300e26091562aa2ccfa6696b57a84539b35df12
Lazar Stanojević <laxsrbija@users.noreply.github.com>
2022-07-08 20:44:31 +0200

    Add a WebFlux module

---
+++ benchmark-webflux/src/main/java/io/github/laxsrbija/benchmarks/webflux/WebfluxApplication.java
@@ -0,0 +1,12 @@
+package io.github.laxsrbija.benchmarks.webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxApplication {
+
+  public static void main(final String[] args) {
+    SpringApplication.run(WebfluxApplication.class, args);
+  }
+}
