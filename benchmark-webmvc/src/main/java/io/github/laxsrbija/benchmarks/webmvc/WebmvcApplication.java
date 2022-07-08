aaaf7adcefcc2065a558c1846628f7b51b025eba
Lazar Stanojević <laxsrbija@users.noreply.github.com>
2022-07-08 20:36:57 +0200

    Add a WebMVC module

---
+++ benchmark-webmvc/src/main/java/io/github/laxsrbija/benchmarks/webmvc/WebmvcApplication.java
@@ -0,0 +1,12 @@
+package io.github.laxsrbija.benchmarks.webmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebmvcApplication {
+
+  public static void main(final String[] args) {
+    SpringApplication.run(WebmvcApplication.class, args);
+  }
+}
