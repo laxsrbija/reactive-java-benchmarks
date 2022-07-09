38cf7cf9f68796cf29ef95a65ad95777ee0696ae
Lazar Stanojević <laxsrbija@users.noreply.github.com>
2022-07-09 16:47:34 +0200

    Add endpoints that call remote services

--- benchmark-webflux/src/main/java/io/github/laxsrbija/benchmarks/webflux/BenchmarkRestController.java
+++ benchmark-webflux/src/main/java/io/github/laxsrbija/benchmarks/webflux/BenchmarkRestController.java
@@ -1,22 +1,39 @@
 package io.github.laxsrbija.benchmarks.webflux;
 
+import java.net.URI;
 import java.time.Duration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 @RestController
 public class BenchmarkRestController {
 
   @GetMapping("text")
-  public String text() {
-    return "Hello world!";
+  public Mono<String> text() {
+    return Mono.just("Hello world!");
   }
 
   @GetMapping("text/{delay}")
-  public Mono<String> textWithDelay(@PathVariable("delay") long delay) {
+  public Mono<String> textWithDelay(@PathVariable("delay") final long delay) {
     return Mono.just("Hello world!")
         .delayElement(Duration.ofMillis(delay));
+  }
+
+  @GetMapping("remote/{localDelay}")
+  public Mono<String> remoteWithDelay(
+      @PathVariable("localDelay") final long localDelay,
+      @RequestParam("server") final String server) {
+    final URI uri = URI.create(server);
+
+    return WebClient.create()
+        .get()
+        .uri(uri)
+        .retrieve()
+        .bodyToMono(String.class)
+        .delayElement(Duration.ofMillis(localDelay));
   }
 }
--- benchmark-webmvc/src/main/java/io/github/laxsrbija/benchmarks/webmvc/BenchmarkRestController.java
+++ benchmark-webmvc/src/main/java/io/github/laxsrbija/benchmarks/webmvc/BenchmarkRestController.java
@@ -1,12 +1,19 @@
 package io.github.laxsrbija.benchmarks.webmvc;
 
+import java.net.URI;
 import java.util.concurrent.locks.LockSupport;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 public class BenchmarkRestController {
+
+  private final RestTemplate restTemplate = new RestTemplate();
 
   @GetMapping("text")
   public String text() {
@@ -17,5 +24,17 @@ public class BenchmarkRestController {
   public String textWithDelay(@PathVariable("delay") long delay) {
     LockSupport.parkNanos(delay * 1_000_000);
     return "Hello world!";
+  }
+
+  @GetMapping("remote/{localDelay}")
+  public String remoteWithDelay(
+      @PathVariable("localDelay") final long localDelay,
+      @RequestParam("server") final String server) {
+    final URI uri = URI.create(server);
+
+    final ResponseEntity<String> response = restTemplate.getForEntity(uri, String.class);
+    LockSupport.parkNanos(localDelay * 1_000_000);
+
+    return response.getBody();
   }
 }
