cdc48fa2ee0bc32b55e7435c9f961b39e63cac5b
Lazar Stanojević <laxsrbija@users.noreply.github.com>
2022-07-09 16:48:26 +0200

    Optimize imports

--- benchmark-webmvc/src/main/java/io/github/laxsrbija/benchmarks/webmvc/BenchmarkRestController.java
+++ benchmark-webmvc/src/main/java/io/github/laxsrbija/benchmarks/webmvc/BenchmarkRestController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 public class BenchmarkRestController {
